8b87b8096d36626a66913c28cbfaa49f3d07c117
kjadlkg <kjadlkg@gmail.com>
2024-12-10 12:40:48 +0900

    canvas & context

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-alert('hello');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d")  // Choose 2D(2d) or 3D(other)
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillRect(50, 50, 100 , 200);
